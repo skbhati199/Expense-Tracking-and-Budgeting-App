adf0850e02bd5128db5048f33d48741617c6165f
sonu kumar <skbhati199@gmail.com>
2025-04-29 20:16:12 +0530

    budget-service

---
+++ budget-service/src/test/java/com/booking/budgetservice/BudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.budgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
