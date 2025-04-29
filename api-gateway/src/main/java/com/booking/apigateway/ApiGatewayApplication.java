58273d229943eaddca6c2a5875ffbefafeba01c3
sonu kumar <skbhati199@gmail.com>
2025-04-29 20:16:50 +0530

    api gateway

--- api-gateway/src/main/java/com/booking/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/booking/apigateway/ApiGatewayApplication.java
@@ -1,16 +1,13 @@
 package com.booking.apigateway;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-
-@SpringBootApplication
-public class ApiGatewayApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+    
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    
+    @SpringBootApplication
+    public class ApiGatewayApplication {
+    
+        public static void main(String[] args) {
+            SpringApplication.run(ApiGatewayApplication.class, args);
+        }
     }
 
-}
